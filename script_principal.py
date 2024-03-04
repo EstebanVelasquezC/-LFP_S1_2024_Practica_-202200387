d5223583e75b482593e4a9fcce30c5a182b0e3d2
Esteban Velasquez <Tibivelcas@gmail.com>
2024-03-04 00:31:08 -0600

    practica 1, error en archivo

--- script_principal.py
+++ script_principal.py
@@ -1,7 +1,7 @@
 from PetManager import PetManager
-
+#Hola
 def show_credits():
-    print("Curso:  LENGUAJES FORMALES Y DE PROGRAMACION")
+    print("Curso: LENGUAJES FORMALES Y DE PROGRAMACION")
     print("Sección: B-")
     print("Carnet: 202200387")
     input("Presiona Enter para continuar...")
@@ -12,14 +12,14 @@ def main():
         pet_manager = PetManager()
         pet_manager_menu(pet_manager)
         
-        choice = input("¿Quieres regresar al Menú Principal? (Sí/No): ").lower()
+        choice = input("¿Quieres regresar al Menú Principal? (Si/No): ").lower()
         if choice != 'si':
             print("Gracias por usar el sistema. ¡Hasta luego!")
             break
 
 def pet_manager_menu(pet_manager):
     while True:
-        print("\nMenú PetManager:")
+        print("\nMenu PetManager:")
         print("1. Crear Gato")
         print("2. Dar de Comer")
         print("3. Jugar")
@@ -28,18 +28,18 @@ def pet_manager_menu(pet_manager):
         print("6. Cargar Archivo")
         print("7. Salir")
 
-        choice = input("Ingrese el número de la opción deseada: ")
+        choice = input("Ingrese el numero de la opcion deseada: ")
 
         if choice == "1":
             pet_name = input("Ingrese el nombre del gato: ")
             pet_manager.create_pet(pet_name)
         elif choice == "2":
             pet_name = input("Ingrese el nombre del gato: ")
-            mouse_weight = input("Ingrese el peso del ratón: ")
+            mouse_weight = input("Ingrese el peso del raton: ")
             pet_manager.eat(pet_name, int(mouse_weight))
         elif choice == "3":
             pet_name = input("Ingrese el nombre del gato: ")
-            minutes = input("Ingrese la duración del juego en minutos: ")
+            minutes = input("Ingrese la duracion del juego en minutos: ")
             pet_manager.play(pet_name, int(minutes))
         elif choice == "4":
             pet_name = input("Ingrese el nombre del gato: ")
@@ -47,13 +47,13 @@ def pet_manager_menu(pet_manager):
         elif choice == "5":
             pet_manager.print_global_summary()
         elif choice == "6":
-            filename = input("Ingrese el nombre del archivo a cargar: ")
-            pet_manager.load_from_file(filename)
+            file_name = input("Ingrese el nombre del archivo: ")
+            pet_manager.load_from_file(file_name)
         elif choice == "7":
-            print("Saliendo del Menú PetManager")
+            print("Saliendo del Menu PetManager")
             break
         else:
-            print("Opción no válida. Por favor, ingrese un número válido.")
+            print("Opcion no valida. Por favor, ingrese un numero valido.")
 
 if __name__ == "__main__":
     main()
